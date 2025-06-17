f0df6d77d88333419cf7d902c73c266ffd6b847e
VitalR <vitalmountlabs@gmail.com>
2025-06-17 16:43:10 +0300

    chore: add Chainlink Functions & Data Feeds configuration for Sepolia

--- config/SepoliaConfig.sol
+++ config/SepoliaConfig.sol
@@ -20,4 +20,17 @@ library SepoliaConfig {
     address public constant MOCK_DAI = 0xF19061331751efd44eCd2E9f49903b7D68651368;
     address public constant MOCK_WETH = 0x7f4eb26422b35D3AA5a72D7711aD12905bb69F59;
     address public constant MOCK_WBTC = 0x4a098CaCd639aE0CC70F6f03d4A01608286b155d;
+
+    // Chainlink Functions Configuration
+    address public constant CHAINLINK_FUNCTIONS_ROUTER = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
+    bytes32 public constant CHAINLINK_DON_ID = 0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+    uint32 public constant CHAINLINK_GAS_LIMIT = 300000;
+    uint64 public constant CHAINLINK_SUBSCRIPTION_ID = 0; // To be set after creating subscription
+
+    // Chainlink Data Feeds (Sepolia Testnet)
+    address public constant ETH_USD_PRICE_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    address public constant BTC_USD_PRICE_FEED = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
+    address public constant LINK_USD_PRICE_FEED = 0xc59E3633BAAC79493d908e63626716e204A45EdF;
+    address public constant DAI_USD_PRICE_FEED = 0x14866185B1962B63C3Ea9E03Bc1da838bab34C19;
+    address public constant USDC_USD_PRICE_FEED = 0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E;
 }
